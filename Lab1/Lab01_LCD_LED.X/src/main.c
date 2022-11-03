c6b557d0d276270ff3214ce1eec2ea73305604cc
Yichao <yichao.gao@tum.de>
2022-11-03 13:30:23 +0100

    solution 1 with comments

--- Lab1/Lab01_LCD_LED.X/src/main.c
+++ Lab1/Lab01_LCD_LED.X/src/main.c
@@ -23,42 +23,42 @@ void delay(){
    for (c = 1; c <= 500; c++)
        for (d = 1; d <= 500; d++)
        {};
-}
+}// definition of delay function
+
 int main(){
     uint16_t i=0;
     lcd_initialize();
     led_init();
-    lcd_clear();
+    lcd_clear();// Clear the Screen and reset the cursor
     
     //Init LCD and LEDs
 while(1){
-    // Clear the Screen and reset the cursor
-    
+    // loop     
     lcd_locate(0, 0);
     lcd_printf("Dorothea");
     lcd_locate(0, 1);
     lcd_printf("Yichao");
     lcd_locate(0, 2);
-    lcd_printf("Giuila");
+    lcd_printf("Giuila");  //print names of group members
     
     lcd_locate(0, 3);
-    lcd_printf("Counter:%d",i);
+    lcd_printf("Counter:%d",i); // print the counter
     
     if((i&0x01)==0x01) SETLED(LED5_PORT); 
     if((i&0x02)==0x02) SETLED(LED4_PORT); 
     if((i&0x04)==0x04) SETLED(LED3_PORT); 
     if((i&0x08)==0x08) SETLED(LED2_PORT); 
-    if((i&0x10)==0x10) SETLED(LED1_PORT); 
+    if((i&0x10)==0x10) SETLED(LED1_PORT); // turn on LED following the order of binary code from the least significant to the highest bit
       
-    delay();
+    delay();// call up delay function
     CLEARLED(LED1_PORT);
     CLEARLED(LED2_PORT);
     CLEARLED(LED3_PORT);
     CLEARLED(LED4_PORT);
-    CLEARLED(LED5_PORT);
-    i++;
+    CLEARLED(LED5_PORT);//close all LEDs
+    i++;//counter +
     }
-    // Stop
+    
 
         ;
 }
