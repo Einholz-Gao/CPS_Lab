915380deb8682e854311c6da990e589f4aa93885
Einholz <yichao.gao@tum.de>
2022-11-16 16:39:01 +0100

    Lab2 but have some problems

--- Lab2/Lab02_Interrupts_Timers.X/src/lab02.c
+++ Lab2/Lab02_Interrupts_Timers.X/src/lab02.c
@@ -11,7 +11,7 @@
 #include "led.h"
 
 #define FCY_EXT 32768
-int T1_counter,T2_counter;
+unsigned int T1_counter,T2_counter;
 void initialize_timer()
 {
     // Enable RTC Oscillator -> this effectively does OSCCONbits.LPOSCEN = 1
@@ -27,13 +27,13 @@ void initialize_timer()
     T2CONbits.TON = 0;//Disable Timer 2
     T3CONbits.TON = 0;//Disable Timer 3
     // Set Prescaler
-    T1CONbits.TCKPS = 0b40;
-    T2CONbits.TCKPS = 0b40;
+    T1CONbits.TCKPS = 0b11;
+    T2CONbits.TCKPS = 0b11;
     T3CONbits.TCKPS = 0b00;
     // Set Clock Source
     T1CONbits.TCS = 1;
     T2CONbits.TCS = 0;
-    T2CONbits.TCS = 0;
+    T3CONbits.TCS = 0;
     // Set Gated Timer Mode -> don't use gating
     CLEARBIT(T1CONbits.TGATE);
     CLEARBIT(T2CONbits.TGATE);
@@ -43,13 +43,14 @@ void initialize_timer()
     // Load Timer Periods
     PR1 = 128;// 1 second starting from 0
     PR2 = 100;// 2 milliseconds
-    PR3 = 100000000000000000;
+    PR3 = 100000000000000;//Problem!!!!!!!!!!!!!!!!!!!!!!
     // Reset Timer Values
     TMR1 = 0x00;
     TMR2 = 0x00;
+    
     // Set Interrupt Priority
     IPC0bits.T1IP = 0x01;
-    IPC0bits.T2IP = 0x01;
+    IPC1bits.T2IP = 0x01;
     // Clear Interrupt Flags
     IFS0bits.T1IF = 0;
     IFS0bits.T2IF = 0;
@@ -59,6 +60,7 @@ void initialize_timer()
     // Enable the Timers
     T1CONbits.TON = 1;
     T2CONbits.TON = 1;
+    T3CONbits.TON = 1;
     
 }
 
@@ -68,18 +70,30 @@ void timer_loop()
     lcd_printf("Lab02: Int & Timer");
     lcd_locate(0, 1);
     lcd_printf("Group: Group7");
+    int T3_counter=0;
+
     TMR3 = 0x00;
     while(TRUE)
     {
+        int T1 = T1_counter/60;
         lcd_locate(0, 2);
-        lcd_printf("%2d:%2d:%3d",T1_counter%60,T1_counter,T2_counter*2);
-      
-        if (TMR3 == 2000){
-            TOGGLELED(LED3_PORT);
-            TMR3 = 0x00;
-            }
+        lcd_printf("%2d:%2d.%3d",T1,T1_counter%60,T2_counter*2%1000);
+        float time = TMR3/12800;
+//        if (T3_counter == 2000){
+//            TOGGLELED(LED3_PORT);
+            lcd_locate(0, 3);
+            lcd_printf("TMR3=%d",TMR3);
+            lcd_locate(0, 4);
+            lcd_printf("time: %f",time);
+//            TMR3=0x00;
+//            T3_counter=0;}
+//        T3_counter++;
     }
 }
+//Problem: 
+//1.TMR3 works not good
+//2.loop works slowly.
+//3.PR3 highest value???
 
 void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
 { // invoked every ??
